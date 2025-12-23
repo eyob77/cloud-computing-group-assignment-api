533cc3f68b930a7205aecb239bffd550f86602c8
Eyob <eyobbirhanu111000@gmail.com>
2025-12-23 22:33:24 +0300

    refactor: clean up code formatting in authControllers and server files

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -2,27 +2,30 @@ import User from '../models/User.js'; // Your Mongoose schema
 import {getAuth, clerkClient} from '@clerk/express';
 
 export const getUser = async (req, res) => {
-  console.log("HI")
-  const { isAuthenticated, userId } = getAuth(req);
+  try {
+    const { isAuthenticated, userId } = getAuth(req);
 
-  if (!isAuthenticated) {
-    return res.status(401).json({ error: 'User not authenticated' });
+    if (!isAuthenticated) {
+      return res.status(401).json({ error: 'User not authenticated' });
+    }
+
+    // Fetch user from Clerk
+    const clerkUser = await clerkClient.users.getUser(userId);
+
+    // Check if user exists in MongoDB
+    let user = await User.findOne({ clerkUserId: userId });
+
+    // If not, create user in MongoDB
+    if (!user) {
+      user = await User.create({
+        clerkUserId: userId,
+        email: clerkUser.emailAddresses[0].emailAddress,
+        username: clerkUser.username || '',
+      });
+    }
+    console.log(user)
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
   }
-
-  // Fetch user from Clerk
-  const clerkUser = await clerkClient.users.getUser(userId);
-
-  // Check if user exists in MongoDB
-  let user = await User.findOne({ clerkUserId: userId });
-
-  // If not, create user in MongoDB
-  if (!user) {
-    user = await User.create({
-      clerkUserId: userId,
-      email: clerkUser.emailAddresses[0].emailAddress,
-      username: clerkUser.username || '',
-    });
-  }
-  console.log(user)
-  res.json(user);
 };
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // RL
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
