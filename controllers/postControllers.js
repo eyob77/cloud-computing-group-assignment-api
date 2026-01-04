9427152cafb93726fdd1d4c004430783ef32cef6
eyob77 <eyobbirhanu111000@gmail.com>
2026-01-04 11:23:57 +0300

    feat: implement multer for image upload in post creation

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,10 +1,19 @@
 export const createPost = async (req, res) => {
-    try {
-        console.log("HI from create post")
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' });
-    }
-}
+  try {
+    console.log("FILES:", req.files); // array
+    console.log("BODY:", req.body);   // { post: 'JSON string' }
+
+    const post = JSON.parse(req.body.post);
+
+    console.log("PARSED POST:", post);
+
+    res.status(200).json({ message: "Post created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
 
 export const getPost = async(req,res)=>{
     try {
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { createPost, getPost } from "../controllers/postControllers.js";
 import { requireAuth } from "@clerk/express";
+import { upload } from "../lib/multer.js";
 
 const router = express.Router();
 
-router.post("/create", requireAuth(), createPost);
+router.post("/create",requireAuth(), upload.array("images"),createPost);
 router.get("/", getPost);
 
 export default router;
