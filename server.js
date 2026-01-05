b3fa704c25cbc35d5f9482da102b662c188a3546
eyob77 <eyobbirhanu111000@gmail.com>
2026-01-05 21:46:17 +0300

    fix: cors issues

--- server.js
+++ server.js
@@ -17,12 +17,21 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL ,
-    credentials: true,
-  })
-);
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(clerkMiddleware())
