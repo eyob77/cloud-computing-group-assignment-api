115b753f26fc49da53afb18e63ce5eaac629b816
eyob77 <eyobbirhanu111000@gmail.com>
2026-01-05 15:53:47 +0300

    feat: integrate Cloudinary for image uploads in post creation and update Post model

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,13 +1,38 @@
+import Post from "../models/Post.js";
+import cloudinary from "../lib/cloudinary.js";
+
 export const createPost = async (req, res) => {
   try {
-    console.log("FILES:", req.files); // array
-    console.log("BODY:", req.body);   // { post: 'JSON string' }
+    const files = req.files || [];
+    const body = req.body.post ? JSON.parse(req.body.post) : {};
 
-    const post = JSON.parse(req.body.post);
+    const uploadedUrls = [];
+    for (const file of files) {
 
-    console.log("PARSED POST:", post);
+      const dataUri = `data:${file.mimetype};base64,${file.buffer.toString("base64")}`;
+      const result = await cloudinary.uploader.upload(dataUri, { folder: "posts" });
+      uploadedUrls.push(result.secure_url || result.url);
 
-    res.status(200).json({ message: "Post created successfully" });
+      body.contents.find(contentBlock => contentBlock.type === "image" && contentBlock.content === file.originalname).url = result.secure_url || result.url;
+    }
+
+    
+
+    const newPost = new Post({
+      author: body.author || "",
+      title: body.title || "",
+      description: body.description || "",
+      date: body.date || new Date().toISOString(),
+      readTime: body.readTime || body.read_time || "",
+      category: body.category || "",
+      image: uploadedUrls.length ? uploadedUrls[0] : body.image || "",
+      contents: body.contents || []
+    });
+
+    await newPost.save();
+    console.log(newPost);
+
+    res.status(201).json({ message: "Post created successfully", post: newPost });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Internal server error" });
--- models/Post.js
+++ models/Post.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
 
+const contentBlockSchema = new mongoose.Schema({
+    type: { type: String, enum: ["paragraph", "list", "heading", "image","subheading"], required: true },
+    content: { type: String, required: true },
+    url: { type: String  }
+}, { _id: false });
+
 const postSchema = new mongoose.Schema({
-    id:{ type: Number, required: true, unique: true },
     author: { type: String, required: true },
     title: { type: String, required: true },
-    description: { type: String, },
+    description: { type: String },
     date: { type: String },
     readTime: { type: String },
     category: { type: String },
-    image_url: { type: String },
-    content: { type: String },
+    image: { type: String },
+    contents: { type: [contentBlockSchema], default: [] }
 }, { timestamps: true });
 
 const Post = mongoose.model("Post", postSchema);
